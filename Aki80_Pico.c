84afa3c89e0af7d79a1500cfdc2a322458152c5d
Norihiro Kumagai <tendai22plus@gmail.com>
2025-05-28 01:00:34 +0900

    bootloading with PIO now debugging, not run properly

--- Aki80_Pico.c
+++ Aki80_Pico.c
@@ -20,7 +20,7 @@
 #define IORQ_Pin    17
 #define RD_Pin      18
 #define A0_Pin      19
-#define RAMCE2_Pin  20
+#define RAMCS2_Pin  20
 #define RAMWE_Pin   21
 #define RESET_Pin   26
 #define WAIT_Pin    27
@@ -29,10 +29,6 @@
 #define TEST_Pin    22
 
 #define TOGGLE() do {    gpio_xor_mask(((uint32_t)1)<<TEST_Pin); } while(0)
-
-void sram_control_forever(PIO pio, uint sm, uint offset) {
-    sram_control_program_init(pio, sm, offset);
-}
 
 void databus_control_forever(PIO pio, uint sm, uint offset) {
     databus_control_program_init(pio, sm, offset);
@@ -54,76 +50,102 @@ void gpio_out_init(uint gpio, bool value) {
     gpio_set_function(gpio, GPIO_FUNC_SIO);
 }
 
-static uint8_t ram16[16] = {
+static uint8_t ram16[4] = {
     0x76
 };
 
 //
 // bootloader ...
 //
+#define INST_MODE 0
+#define READ_MODE 1
+#define WRITE_MODE 2
 
-static uint8_t z80_sram_cycle(int pindir, uint8_t instruction, uint8_t wr_data)
+static int verbose = 0;
+
+static uint8_t z80_machine_cycle(int mode, uint8_t data)
 {
-    uint8_t mask = pindir ? 0xff : 0;   // pindir: true: OUT, false: IN
-    uint8_t data = 0;
+    uint32_t gpio;
     TOGGLE();
-    while(((gpio = gpio_get_all()) & (1<<MREQ_Pin)));    // wait for MREQ cycle start
-    if ((gpio & (1<<RFSH_Pin)) == 0) {
-        // Refresh cycle ... ignored, possible never happens
-    } else if (gpio & (1<<RD_Pin)) {
-        // WR cycle ... enable SRAM and WE asserted
+    pio_sm_get_blocking(pio0, 0);    // wait for access event occurs
+    gpio = gpio_get_all();
+    
+    if (mode == INST_MODE) {
+        // M1 cycle
         gpio_set_dir_masked(0xff, 0xff);    // D0-D7 output
-        gpio_put_masked(0xff, wr_data);
-        gpio_put(RAMCE2_Pin, false);
-        gpio_put(WE_Pin, false);
+        gpio_put_masked(0xff, data);
+        if (verbose) printf("I%02X\n", data);
+    } else if (mode == READ_MODE) {
+        // Memory Read cycle
+        gpio_set_dir_masked(0xff, 0);       // D0-D7 input
+        gpio_put(RAMCS2_Pin, false);        // SRAM enable
         sleep_us(1);
-        gpio_put(WE_Pin, true);
-        gpio_put(RAMCE2_Pin, high);
+        data = gpio_get_all() & 0xff;       // read sram data
+        if (verbose) printf("R%02X\n", data);
+    } else if (mode == WRITE_MODE) {
+        // Memry Write cycle
+        gpio_set_dir_masked(0xff, 0);       // D0-D7 input
+        // no databus output, let Z80 to write SRAM
+        gpio_put(RAMWE_Pin, false);
+        gpio_put(RAMCS2_Pin, true);
+        sleep_us(1);        // wait for SRAM data settled down
+        gpio_put(RAMWE_Pin, true);
+        gpio_put(RAMCS2_Pin, true);
+        data = gpio_get_all() & 0xff;       // read databus data
+        if (verbose) printf("W%02X\n", data);
     }
+    pio_sm_put(pio0, 0, 0);         // release WAIT machine
+    pio_sm_get_blocking(pio0, 0);   // wait for MREQ negated
+    gpio_set_dir_masked(0xff, 0);           // D0-D7 Hi-Z
+    pio_sm_put(pio0, 0, 0);
     TOGGLE();
-    // post process
-    if ((gpio & (1<<RD_Pin)) == 0) {
-        // RD cycle, put instruction on D0-D7
-        gpio_set_dir_masked(0xff, 0xff);    // D0-D7 output
-        gpio_put_masked(0xff, instruction);
-    }
-    gpio_put(BUSRQ_Pin, false);         // a BUSRQ trap is prepared
-    gpio_put(WAIT_Pin, true);           // restart Z90
-    while ((gpio_get_all() & (1<<BUSRQ_Pin)));  // wait for BUSAK assert 
-    gpio_set_dir_masked(0xff, 0);       // D0-D7 input
-    gpio_put(WAIT_Pin, false);          // for next cycle
-    gpio_put(BUSRQ_Pin, true);          // restart Z80
+    return data;
+}
+
+void z80_set_addr(uint16_t addr)
+{
+    gpio_put(RESET_Pin, false);     // reset Z80
+    sleep_us(1);
+    gpio_put(RESET_Pin, true);      // restart
+    z80_machine_cycle(INST_MODE, 0x21);     // LD HL,addr
+    z80_machine_cycle(INST_MODE, addr&0xff);
+    z80_machine_cycle(INST_MODE, (addr>>8)&0xff);
+}
+
+void z80_write_byte(uint8_t data)
+{
+    z80_machine_cycle(INST_MODE, 0x3e);     // LD A,data
+    z80_machine_cycle(INST_MODE, data);
+    z80_machine_cycle(INST_MODE, 0x77);     // LD (HL),A
+    z80_machine_cycle(WRITE_MODE, 0);
+    z80_machine_cycle(INST_MODE, 0x23);     // INC HL
+}
+
+uint8_t z80_read_byte(void)
+{
+    uint8_t data;
+    z80_machine_cycle(INST_MODE, 0x7e);     // LD A,(HL)
+    data = z80_machine_cycle(READ_MODE, 0);
+    z80_machine_cycle(INST_MODE, 0x23);     // INC HL
     return data;
 }
 
 void boot(uint16_t start, uint8_t *buf, int length)
 {
     uint8_t data;
-    gpio_out_init(OE_Pin, true);
-    gpio_out_init(WE_Pin, true);
 
-    gpio_put(WAIT_Pin, false);
-    gpio_put(RESET_Pin, true);  // start it
-    z80_sram_cycle(-1, 0xc3, 0);
-    z80_sram_cycle(-1, (start)&0xff, 0);
-    z80_sram_cycle(-1, (start>>8)&0xff, 0);
-    for (int i = 0; i < sizeof ram16; ++i) {
-        z80_sram_cycle(1, 0, ram16[i]);
+    printf("boot: start = %04X, length = %d\n", start, length);
+    z80_set_addr(start);
+    for (int i = 0; i < length; ++i) {
+        z80_write_byte(buf[i]);
     }
-    // read sram
-    gpio_put(RESET_Pin, false);
-    sleep_ms(1);
-    gpio_put(WAIT_Pin, false);
-    gpio_put(RESET_Pin, true);  // restart it
-    z80_sram_cycle(-1, 0xc3, 0);
-    z80_sram_cycle(-1, (start)&0xff, 0);
-    z80_sram_cycle(-1, (start>>8)&0xff, 0);
-    for (int i = 0; i < sizeof ram16; ++i) {
-        data = z80_sram_cycle(0, 0, 0);
-        if (i % 8 == 0)
+    z80_set_addr(start);
+    for (int i = 0; i < length; ++i) {
+        data = z80_read_byte();
+        if ((i % 8) == 0)
             printf("%04X ", start + i);
         printf("%02X ", data);
-        if (i % 8 == 7)
+        if ((i % 8) == 7)
             printf("\n");
     }
     gpio_put(RESET_Pin, false);  // reset again
@@ -131,6 +153,17 @@ void boot(uint16_t start, uint8_t *buf, int length)
 
 int main()
 {
+    stdio_init_all();
+    // Set up our UART
+    uart_init(UART_ID, BAUD_RATE);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
+    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
+
+    uart_puts(UART_ID, " Hello, UART!\r\n");
+    printf("Hello, printf\n");
+
     // test pin, scratch its initial point
     gpio_out_init(TEST_Pin, true);
     TOGGLE();
@@ -140,16 +173,9 @@ int main()
     gpio_out_init(WAIT_Pin, true);
     gpio_out_init(RESET_Pin, false);
     gpio_out_init(BUSRQ_Pin, true);
-    gpio_out_init(RAMCE2_Pin, false);
+    gpio_out_init(RAMCS2_Pin, false);
     gpio_out_init(RAMWE_Pin, true);
 
-    stdio_init_all();
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
     //TOGGLE();
 
@@ -172,53 +198,27 @@ int main()
     uint offset1;
     offset1 = pio_add_program(pio0, &databus_control_program);
     printf("databus_control_program at %d\n", offset1);
-    databus_control_pin_forever(pio0, 0, offset1);
+    databus_control_forever(pio0, 0, offset1);
 
     // Use some the various UART functions to send out data
     // In a default system, printf will also output via the default UART
     
     // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\r\n");
 
     // test program 1: Z80 Free Run
     // start CPU and examine signal transition
+    pio_sm_set_enabled(pio0, 0, true);
+    sleep_us(10);
+    TOGGLE();
+    TOGGLE();
+
+    pio_sm_clear_fifos(pio0, 0);
     gpio_put(WAIT_Pin, true);
     gpio_put(RESET_Pin, true);
 
-    // infinite loop
-    while(true);
 
     // boot ... expand Z80 program to SRAM
     boot(0, &ram16[0], sizeof ram16);
-
-    // setup sram_control PIO sm
-    offset1 = pio_add_program(pio_clock, &sram_control_program);
-    printf("sram_control_program at %d\n", offset1);
-    sram_control_forever(pio_clock, 1, offset1);
-    pio_sm_set_enabled(pio_clock, 1, true);
-    // restart CPU
-    gpio_put(WAIT_Pin, true);
-    gpio_put(RESET_Pin, true);
-
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
-
-    int count = 3;    
-    while (true) {
-        data = pio_sm_get_blocking(pio0, 0);    // wait for MREQ cycle start
-        status = gpio_get_all() >> 14;
-        // bit0: MREQ
-        // bit1: RFSH
-        // bit2: BUSAK
-        // bit3: IORQ
-        // bit4: RD
-        // bit5: A0
-        if ((status & (1<<4)) == 0) {   // RD cycle
-            gpio_set_dir_masked(0xff, 0xff);    // D0-D7 output
-            gpio_put_masked(0xff, 0);   // put NOP
-            pio_sm_put(pio0, 0, 0);
-            pio_sm_get_blocking(pio0, 0);   // wait for MREQ cycle end
-            gpio_set_dir_masked(0xff, 0);   // D0-D7 input
-        }
-
-    }
+    // infinite loop
+    while(true);
 }
